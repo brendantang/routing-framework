156d2d3ef23b46e06c3187126b334af6aef10968
Brendan Tang <b@brendantang.net>
2022-07-21 19:47:32 -0400

    Middlewares

--- example.ts
+++ example.ts
@@ -1,18 +1,23 @@
-import { serve , RouteHandler} from "./mod.ts";
+import { GET, POST, RouteHandler, serve } from "./mod.ts";
 
-const helloHandler: RouteHandler = (req, connInfo, params) => {
-  const name = params["name"]
+const helloHandler: RouteHandler = (_req, _connInfo, params) => {
+  const name = params["name"];
   return new Response("Hello " + name);
 };
 
-
 const routes = {
-  "/hello/:name": helloHandler,
-  "/world": (req) => {
+  "/hello/:name": GET(helloHandler),
+  "/world": () => {
     return new Response("world");
   },
-  "/err": (req) => {
-    throw "Intentional error!"
+  "/echo": POST(async function (
+    req: Request,
+  ) {
+    const body = await req.text();
+    return new Response(body);
+  }),
+  "/err": () => {
+    throw "Intentional error!";
     return new Response("You won't see this");
   },
 };
--- mod.ts
+++ mod.ts
@@ -1,11 +1,10 @@
 import {
+  ConnInfo,
   serve as stdServe,
   ServeInit,
-  ConnInfo
 } from "https://deno.land/std@0.149.0/http/server.ts";
 export type { ConnInfo } from "https://deno.land/std@0.149.0/http/server.ts";
-import { logger } from "./logger.ts"
-
+import { logger } from "./logger.ts";
 
 export type RouteHandler = (
   req: Request,
@@ -16,19 +15,28 @@ export type RouteHandler = (
 export interface Routes {
   [path: string]: RouteHandler;
 }
-export type RouteParams = Record<string,string>
+export type RouteParams = Record<string, string>;
+
+export type Middleware = (next: RouteHandler) => RouteHandler;
 
 export function serve(
   routes: Routes,
+  middlewares: Middleware[] = [logger],
   options: ServeInit = defaultServeOptions,
 ): void {
   stdServe(async (req: Request, connInfo: ConnInfo): Promise<Response> => {
-    const { search, pathname } = new URL(req.url);
+    const { pathname } = new URL(req.url);
     for (const route of Object.keys(routes)) {
       const pattern = new URLPattern({ pathname: route });
       if (pattern.test({ pathname })) {
         const params = pattern.exec({ pathname })?.pathname.groups || {};
-        const response = await logger(routes[route])(req, connInfo, params);
+        const handlerWithMiddlewares = middlewares.reduce(
+          function (previous, current) {
+            return (current(previous));
+          },
+          routes[route],
+        );
+        const response = await handlerWithMiddlewares(req, connInfo, params);
         return response;
       }
     }
@@ -44,4 +52,20 @@ export const defaultErrorHandler = (err: unknown) => {
 
 export const defaultServeOptions = { port: 8000, onError: defaultErrorHandler };
 
+export const GET: Middleware = makeMiddlewareForMethod("GET");
+export const POST: Middleware = makeMiddlewareForMethod("POST");
+export const PATCH: Middleware = makeMiddlewareForMethod("PATCH");
+export const DELETE: Middleware = makeMiddlewareForMethod("DELETE");
 
+function makeMiddlewareForMethod(
+  method: "GET" | "POST" | "PATCH" | "DELETE",
+): Middleware {
+  return function (next: RouteHandler) {
+    return function (req, connInfo, params) {
+      if (req.method === method) {
+        return next(req, connInfo, params);
+      }
+      return new Response("Method not allowed", { status: 405 });
+    };
+  };
+}
