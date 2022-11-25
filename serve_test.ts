e3169045f36b70e0f90901b14540d2e22a90d4bf
Brendan Tang <b@brendantang.net>
2022-11-25 16:01:30 -0500

    More robust testing

--- serve_test.ts
+++ serve_test.ts
@@ -5,16 +5,37 @@ const routes: Routes = {
   "/hello": () => {
     return new Response("World");
   },
+  "/foo": () => {
+    return new Response("bar");
+  },
+};
+
+const assertResponse = async (
+  path: string,
+  method: string,
+  status: number,
+  body?: string,
+) => {
+  const response = await handler(
+    new Request(`http://localhost${path}`, { method }),
+  );
+  assertEquals(
+    status,
+    response.status,
+  );
+  if (body) {
+    const got = await response.text();
+    assertEquals(
+      body,
+      got,
+    );
+  }
 };
 
 const handler = handle(routes);
 Deno.test("serve", async (t) => {
-  await t.step("handle", async () => {
-    const response = await handler(new Request("http://localhost/hello"));
-    const body = await response.text();
-    assertEquals(
-      "World",
-      body,
-    );
+  await t.step("handle the correct route", async () => {
+    await assertResponse("/hello", "GET", 200, "World");
+    await assertResponse("/foo", "GET", 200, "bar");
   });
 });
